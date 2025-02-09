292a90991050e65b027584eef698512854fe4a70
eparlato <edoardo.parlato@qmates.tech>
2025-02-09 21:58:43 +0100

    Introduce OCRNumber
    This object is initialiaed with an OCR representation of a number (3 lines).
    
    It returns a number representation of itself.

---
+++ src/ocr-number.ts
@@ -0,0 +1,90 @@
+export class OCRNumber {
+  
+  private ocrRepresentation: string[];
+
+  constructor(ocrRepresentation: string[] ) {
+    this.ocrRepresentation = ocrRepresentation
+  }
+
+  static from(ocrRepresentation: string[]) {
+   return new OCRNumber(ocrRepresentation)
+  }
+
+  toNumber(): number | undefined {
+   return this.ocrRepresentationToNumbers.get(this.ocrRepresentation.join("\n"))
+  }
+
+  private ocrRepresentationToNumbers: Map<string, number> = new Map([
+   [
+    [
+      "   ",
+      "  |",
+      "  |"
+    ].join("\n"), 1
+   ],
+   [
+    [
+      " _ ",
+      " _|",
+      "|_ "
+    ].join("\n"), 2
+   ],
+   [
+    [
+      " _ ",
+      " _|",
+      " _|"
+    ].join("\n"), 3
+   ],
+   [
+    [
+      "   ",
+      "|_|",
+      "  |"
+    ].join("\n"), 4
+   ],
+   [
+    [
+      " _ ",
+      "|_ ",
+      " _|"
+    ].join("\n"), 5
+   ],
+   [
+    [
+      " _ ",
+      "|_ ",
+      "|_|"
+    ].join("\n"), 6
+   ],
+   [
+    [
+      " _ ",
+      "  |",
+      "  |"
+    ].join("\n"), 7
+   ],
+   [
+    [
+      " _ ",
+      "|_|",
+      "|_|"
+    ].join("\n"), 8
+   ],
+   [
+    [
+      " _ ",
+      "|_|",
+      " _|"
+    ].join("\n"), 9
+   ],
+   [
+    [
+      " _ ",
+      "| |",
+      "|_|"
+    ].join("\n"), 0
+   ]
+  ])
+  
+}
---
+++ test/ocr-number.test.ts
@@ -0,0 +1,124 @@
+import { describe, expect, it } from "vitest";
+import { OCRNumber } from "../src/ocr-number"
+
+describe("OCRNumber", () => {
+  it("returns a number from OCR representation of value 1", () => {
+    const ocrRepresentation = [
+      "   ",
+      "  |",
+      "  |"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(1)
+  })
+
+  it("returns a number from OCR representation of value 2", () => {
+    const ocrRepresentation = [
+      " _ ",
+      " _|",
+      "|_ "
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(2)
+  })
+
+  it("returns a number from OCR representation of value 3", () => {
+    const ocrRepresentation = [
+      " _ ",
+      " _|",
+      " _|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(3)
+  })
+
+  it("returns a number from OCR representation of value 4", () => {
+    const ocrRepresentation = [
+      "   ",
+      "|_|",
+      "  |"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(4)
+  })
+
+  it("returns a number from OCR representation of value 5", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "|_ ",
+      " _|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(5)
+  })
+
+  it("returns a number from OCR representation of value 6", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "|_ ",
+      "|_|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(6)
+  })
+
+  it("returns a number from OCR representation of value 7", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "  |",
+      "  |"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(7)
+  })
+
+  it("returns a number from OCR representation of value 8", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "|_|",
+      "|_|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(8)
+  })
+
+  it("returns a number from OCR representation of value 9", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "|_|",
+      " _|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(9)
+  })
+
+  it("returns a number from OCR representation of value 0", () => {
+    const ocrRepresentation = [
+      " _ ",
+      "| |",
+      "|_|"
+    ]
+    
+    const ocrNumber = OCRNumber.from(ocrRepresentation)
+
+    expect(ocrNumber.toNumber()).to.equal(0)
+  })
+})
