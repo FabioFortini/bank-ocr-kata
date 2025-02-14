0a633fa8f4f6219f9cf738456e18a4580d3a1fb4
eparlato <edoardo.parlato@qmates.tech>
2025-02-14 16:01:28 +0100

    Parse OCR input and finally extract account numbers from it

--- src/account-number-ocr.ts
+++ src/account-number-ocr.ts
@@ -1,6 +1,13 @@
+import { OCRInput } from "./ocr-input"
+
 export class AccountNumberOCR {
 
-    parse(input: string) {
-        return []
-    }
+  parse(input: string) {
+    const ocrInput = new OCRInput(input)
+
+    const entries = ocrInput.extractEntries()
+    console.info('entries:', entries)
+    const result = entries.map(entry => entry.toAccountNumber())
+    return result
+  }
 }
--- test/account-number-ocr.test.ts
+++ test/account-number-ocr.test.ts
@@ -2,21 +2,20 @@ import {describe, expect, it} from "vitest";
 import {AccountNumberOCR} from "../src/account-number-ocr";
 
 describe('Account number ocr', () => {
-    it.skip('read account numbers', () => {
-        const input = `
-    _  _     _  _  _  _  _ 
-  | _| _||_||_ |_   ||_||_|
-  ||_  _|  | _||_|  ||_| _|
-                           
-    _  _     _  _  _  _  _ 
-  | _| _||_||_ |_   || ||_|
-  ||_  _|  | _||_|  ||_| _|
-                           
-`
-        const cleanInput = input.substring(1, input.length - 1)
+    it('read account numbers', () => {
+        const input =
+        "    _  _     _  _  _  _  _ \n" +
+        "  | _| _||_||_ |_   ||_||_|\n" +
+        "  ||_  _|  | _||_|  ||_| _|\n" +
+        "                           \n" +
+        "    _  _     _  _  _  _  _ \n" +
+        "  | _| _||_||_ |_   || ||_|\n" +
+        "  ||_  _|  | _||_|  ||_| _|\n" +
+        "                           \n" 
+
         const ocr = new AccountNumberOCR()
 
-        const parsedInput = ocr.parse(cleanInput)
+        const parsedInput = ocr.parse(input)
 
         expect(parsedInput).toStrictEqual(["123456789", "123456709"])
     })
