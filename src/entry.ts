b40b70fff54b6e1433a9a17088fa9870d6d6383c
eparlato <edoardo.parlato@qmates.tech>
2025-02-15 12:54:42 +0100

    Refactor Entry#toAccountNumber method

--- src/entry.ts
+++ src/entry.ts
@@ -1,60 +1,45 @@
 import { OCRNumber } from "./ocr-number";
 
 export class Entry {
-  private lines: string[] = [];
+  private entryLines: string[] = [];
 
-  constructor(lines: string[]) {
-    this.lines = lines
+  constructor(entryLines: string[]) {
+    this.entryLines = entryLines
   }
 
-  static of(input: string[]) {
-    return new Entry(input);
+  static of(entryLines: string[]) {
+    return new Entry(entryLines);
   }
 
-  toAccountNumber(): any {
-    // TODO refactor
-    const numberOne: string[] = []
-    const numberTwo: string[] = []
-    const numberThree: string[] = []
-    const numberFour: string[] = []
-    const numberFive: string[] = []
-    const numberSix: string[] = []
-    const numberSeven: string[] = []
-    const numberEight: string[] = []
-    const numberNine: string[] = []
+  toAccountNumber(): string {
+    const linesWithSymbols = this.extractLinesWithSymbols()
+    let accountNumber = ""
+    let offset = 0
+    const singleNumberLength = 3
+    const totNumbersOnSingleLine = 9;
 
+    for (let index = 0; index < totNumbersOnSingleLine; index++) {
+      const currentNumber = linesWithSymbols.map(line => {
+        return line.substring(offset, offset + singleNumberLength)
+      })
+      accountNumber += OCRNumber.from(currentNumber).toNumber()
 
-    this.lines.slice(0, 3).forEach(line => {
-      numberOne.push(line.substring(0, 3))
-      numberTwo.push(line.substring(3, 6))
-      numberThree.push(line.substring(6, 9))
-      numberFour.push(line.substring(9, 12))
-      numberFive.push(line.substring(12, 15))
-      numberSix.push(line.substring(15, 18))
-      numberSeven.push(line.substring(18, 21))
-      numberEight.push(line.substring(21, 24))
-      numberNine.push(line.substring(24, 27))
-    })
-    const first = OCRNumber.from(numberOne)
-    const second = OCRNumber.from(numberTwo)
-    const third = OCRNumber.from(numberThree)
-    const fourth = OCRNumber.from(numberFour)
-    const fifth = OCRNumber.from(numberFive)
-    const sixth = OCRNumber.from(numberSix)
-    const seventh = OCRNumber.from(numberSeven)
-    const eight = OCRNumber.from(numberEight)
-    const ninth = OCRNumber.from(numberNine)
+      offset += singleNumberLength
+    }
 
-    const accountNumber = `${first.toNumber()}${second.toNumber()}${third.toNumber()}${fourth.toNumber()}${fifth.toNumber()}${sixth.toNumber()}${seventh.toNumber()}${eight.toNumber()}${ninth.toNumber()}`;
     return accountNumber
   }
 
+  private extractLinesWithSymbols() {
+    return this.entryLines.slice(0, 3);
+  }
+
   equals(other: Entry): boolean {
-    if (this.lines.length !== other.lines.length) {
+    if (this.entryLines.length !== other.entryLines.length) {
       return false;
     }
-    for (let i = 0; i < this.lines.length; i++) {
-      if (this.lines[i] !== other.lines[i]) {
+    for (let i = 0; i < this.entryLines.length; i++) {
+      if (this.entryLines[i] !== other.entryLines[i]) {
         return false;
       }
     }
